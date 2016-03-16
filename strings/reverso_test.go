d563647b9732718a5ee77f50954beb1d6ca6aac5
Richard Natal <bigous@gmail.com>
2016-03-16 11:03:27 -0300

    Criado caso de teste

---
+++ strings/reverso_test.go
@@ -0,0 +1,18 @@
+package strings
+
+import "testing";
+
+func TestReverso(this *testing.T) {
+	var casos = []struct { entrada, esperado string } {
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	};
+	var retorno string;
+	for _, c := range casos {
+		retorno = Reverso(c.entrada);
+		if (retorno != c.esperado) {
+			this.Errorf("Reverso(%q) == %q, esperado %q", c.entrada, retorno, c.esperado);
+		}
+	}
+}
